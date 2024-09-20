c6addebacefb1ac07aaef65d3b0013a3ef302197
banhbao0311 <91855331+banhbao0311@users.noreply.github.com>
2024-09-20 17:50:12 +0700

    Update OrderDetails.cs

--- TechWizWebApp.Domain/OrderDetails.cs
+++ TechWizWebApp.Domain/OrderDetails.cs
@@ -13,5 +13,6 @@ namespace TechWizWebApp.Domain
         public Order? order { get; set; }
         public int variant_id { get; set; }
         public Variant? variant { get; set; }
+        public int quanity { get; set; }
     }
 }
